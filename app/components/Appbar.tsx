0d2fcb4ccf50ab5010bae1edee1d879df5eb8bac
Pratik <pratikyadav3949@gmail.com>
2024-09-07 17:21:50 +0530

    feat(OAuth) add nextauth

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+
+const Appbar = () => {
+  const session = useSession();
+  return (
+    <nav className="flex justify-between items-center mx-auto bg-[#E4E4E7] rounded-b-3xl p-6">
+      <div className="text-3xl">
+        <span className="text-blue-500 font-semibold">Melody</span>Hub
+      </div>
+      <div>
+        {session.data?.user && (
+          <button
+            className="bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-600 rounded-xl p-3"
+            onClick={() => signOut()}
+          >
+            Logout
+          </button>
+        )}
+        {!session.data?.user && (
+          <button
+            className="bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-600 rounded-xl p-3"
+            onClick={() => signIn()}
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Appbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Appbar from "./components/Appbar";
 
 export default function Home() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <Appbar />
+    </div>
+  );
 }
