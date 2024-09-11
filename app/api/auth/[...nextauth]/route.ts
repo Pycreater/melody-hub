b88ff20ab09e7fa6318153d5cab73e9ebee43593
Pratik <pratikyadav3949@gmail.com>
2024-09-11 06:28:29 +0530

    feat(schema) update schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { NextResponse } from "next/server";
 import GoogleProvider from "next-auth/providers/google";
+import { prismaClient } from "@/app/lib/db";
 
 const handler = NextAuth({
   providers: [
@@ -9,6 +10,20 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn(params) {
+      console.log(params);
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: "",
+            provider: "Google",
+          },
+        });
+      } catch (error) {}
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
